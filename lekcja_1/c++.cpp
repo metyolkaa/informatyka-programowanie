7a843c750467c1294f29dc810b92c317849a34c4
metyolkaa <aametolkina@gmail.com>
2023-10-25 11:53:23 +0200

    Updated readme

--- lekcja_1/c++.cpp
+++ lekcja_1/c++.cpp
@@ -1,8 +1,15 @@
  #include <iostream>
  using namespace std;
  int main(){
+short liczba = 2;
+float liczbauzytkownika;
 cout<<"Hello World"<<endl;
-cout << "Imie" << endl;
+cout << "Imie" << liczba <<endl;
+cout << endl;
+cout << "Podaj liczbe:" << endl;
+cin >> liczbauzytkownika;
+cout<< "Twoja liczba" << liczbauzytkownika<< endl;
+
 
     return 0;
 }
