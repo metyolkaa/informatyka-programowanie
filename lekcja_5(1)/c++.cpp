7ab39d0e537fc1ba68e13aa6af806c979a9c462f
metyolkaa <aametolkina@gmail.com>
2023-11-20 15:01:43 +0100

    Updated readme

---
+++ lekcja_5(1)/c++.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	short a =30;
+	while(a>0){
+		cout << a <<",";
+		a -= 1;
+	}
+	cout << endl;
+	short liczba = 0;
+	while(liczba |= 5){
+		cout << "podaj liczbe: ";
+		cin >> liczba;
+	}
+	 cout << endl << endl;
+    short liczba2;
+    do{
+        cout << "Podaj liczbe: ";
+        cin >> liczba2;
+    }while(liczba2 != 5 );
+
+	
+	cout <<endl;
+	system("pause");
+}
